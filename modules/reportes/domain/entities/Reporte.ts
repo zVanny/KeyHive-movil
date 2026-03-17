2a4cf888f545c59f3c703e878b571086afb52d79
Vanny <digitalbyvc@gmail.com>
2026-03-17 07:41:19 -0600

    reports working FAHHHH

--- modules/reportes/domain/entities/Reporte.ts
+++ modules/reportes/domain/entities/Reporte.ts
@@ -1,7 +1,14 @@
 export interface Reporte {
-    id: number;
-    user_id: string;
-    no_casillero?: number;
-    fecha: string;
-    reporte: string;
-  }
+  id: number;
+  userId: string;
+  noCasillero: number | null;
+  fecha: string; // YYYY-MM-DD
+  reporte: string;
+  createdAt: string;
+}
+
+export interface CreateReporteDto {
+  noCasillero: number | null;
+  fecha: string; // YYYY-MM-DD
+  reporte: string;
+}
